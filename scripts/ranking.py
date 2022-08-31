f00d9450e085833fb29df3c0ac9c931d17abc052
emdomingo <domingo.edwardjohn@gmail.com>
2022-08-31 14:25:04 +0800

    Update and run rankings.py to get tournament rankings

--- scripts/ranking.py
+++ scripts/ranking.py
@@ -16,21 +16,32 @@ for div in division:
     r = s.get(url)
     r.html.render()
 
-    rows = r.html.find('table.basic tr.vbw-o-table__row')
-    rank = 1    
-    for row in rows:
-        data=[div.title()]
-        data.append(rank)
-        data.append(row.find('div.vbw-mu__team__name', first=True).text)
-        data.append(int(row.find('td.matcheswon', first=True).text))
-        data.append(int(row.find('td.matcheslost', first=True).text))
-        data.append(int(row.find('td.matchpoints', first=True).text))
-        data.append(float(row.find('td.setsratio', first=True).text))
-        data.append(float(row.find('td.pointsratio', first=True).text))
+    phase = ['pool', 'final']
+    
+    for i in phase:
         
-        rankings.append(data)
-        rank += 1
+        if i == 'pool':
+            table=r.html.find('div[data-tab="round-p"] table.basic', first=True)
+        elif i == 'final':
+            table=r.html.find('div[data-tab="round-f"]', first=True)
         
-cols = ['division', 'rank', 'team', 'won', 'lost', 'points', 'sets_ratio', 'points_ratio']
+        rows = table.find('tr.vbw-o-table__row')
+        rank = 1    
+        for row in rows:
+            data=[div.title()]
+            data.append(i.title())
+            data.append(rank)
+            data.append(row.find('div.vbw-mu__team__name', first=True).text)
+            if i == 'pool':
+                data.append(int(row.find('td.matcheswon', first=True).text))
+                data.append(int(row.find('td.matcheslost', first=True).text))
+                data.append(int(row.find('td.matchpoints', first=True).text))
+                data.append(float(row.find('td.setsratio', first=True).text))
+                data.append(float(row.find('td.pointsratio', first=True).text))
+            
+            rankings.append(data)
+            rank += 1
+        
+cols = ['division', 'phase', 'rank', 'team', 'won', 'lost', 'points', 'sets_ratio', 'points_ratio']
 df = pd.DataFrame(rankings, columns=cols)
-df.to_csv('rankings.csv', index=False)
+df.to_csv('./data/rankings.csv', index=False)
